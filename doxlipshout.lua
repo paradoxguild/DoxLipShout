b95bbe5044b622d3e5e1e8154b0e21e33bce3d62
= <paradox.thunderfury@gmail.com>
2024-09-11 21:35:59 -0700

    Change logic to display those able to use a lipshout within the next 30s

--- doxlipshout.lua
+++ doxlipshout.lua
@@ -8,6 +8,8 @@ local SHOUT_CD = (60 * 10);
 
 local dragging = false;
 local debug = false;
+local hideIfTimers = true;
+local showTimerWhenNear = true;
 
 -- potionTimers = {};
 -- shoutTimers = {};
@@ -93,6 +95,11 @@ end
 local function getInstanceFriendlyUnits()
     units = {};
     unitCount = 0;
+    if debug then
+        unitCount = unitCount + 2;
+        table.insert(units, "Adebug");
+        table.insert(units, "Zdebug");
+    end
     if UnitExists("player") and isWarrior("player") then
         unitName, unitRealm = UnitName("player");
         unitCount = unitCount + 1;
@@ -135,24 +142,48 @@ end
 
 local function buildText()
     output = "";
-    for key, value in pairs(potionTimers) do
+    keys = {};
+    for key, _ in pairs(potionTimers) do
+        table.insert(keys, key);
+    end
+    table.sort(keys);
+
+    for _, key in ipairs(keys) do
+        value = potionTimers[key];
         if value ~= nil then
             displayKey = getDisplayName(key);
-            if string.len(output) > 0 then
-                output = output .. "\n";
-            end
+            showEntry = true;
+            potionNear = false;
+            shoutNear = false;
             duration = value["time"] ~= nil and math.floor(GetTime() - value["time"] + 0.5) or POTION_CD;
             potionCDText = duration < POTION_CD and colorText("N " .. (POTION_CD - duration) .. "s", "FFFF0000") or colorText("Y", "FF008000");
             shoutCDText = colorText("Y", "FF008000");
+            if POTION_CD - duration < 30 then
+                potionNear = true;
+            end
+            if duration < POTION_CD then
+                showEntry = false;
+            end
             if shoutTimers[key] ~= nil then
                 shoutDuration = shoutTimers[key]["time"] ~= nil and math.floor(GetTime() - shoutTimers[key]["time"]) or SHOUT_CD;
+                
+                if SHOUT_CD - shoutDuration < 30 then
+                    shoutNear = true;
+                end
                 if shoutDuration < SHOUT_CD then
+                    showEntry = false;
                     shoutCDText = colorText("N " .. (SHOUT_CD - shoutDuration) .. "s", "FFFF0000");
                 end
             end
-            output = output .. displayKey .. ": Potion " .. potionCDText .. " | Shout " .. shoutCDText;
+            if not hideIfTimers or showEntry or (showTimerWhenNear and potionNear and shoutNear) then
+                if string.len(output) > 0 then
+                    output = output .. "\n";
+                end
+                output = output .. displayKey .. ": Potion " .. potionCDText .. " | Shout " .. shoutCDText;
+            end
         end
     end
+    
     return output;
 end
 
@@ -180,14 +211,16 @@ local function populateTimerPlaceholders()
         end
     end
     -- Remove old units
-    for unitKey, _ in pairs(potionTimers) do
-        if not hasValue(friendlyUnits, unitKey) then
-            potionTimers[unitKey] = nil;
+    if not debug then
+        for unitKey, _ in pairs(potionTimers) do
+            if not hasValue(friendlyUnits, unitKey) then
+                potionTimers[unitKey] = nil;
+            end
         end
-    end
-    for unitKey, _ in pairs(shoutTimers) do
-        if not hasValue(friendlyUnits, unitKey) then
-            shoutTimers[unitKey] = nil;
+        for unitKey, _ in pairs(shoutTimers) do
+            if not hasValue(friendlyUnits, unitKey) then
+                shoutTimers[unitKey] = nil;
+            end
         end
     end
 end
@@ -205,6 +238,9 @@ frame:SetScript("OnEvent", function(self, event, arg1, arg2, arg3, arg4)
         spellName = strtrim(spellName);
 
         if arg1 == "player" and debug then
+            potionTimers[key]["time"] = GetTime();
+            potionTimers["Adebug"]["time"] = GetTime() - 60;
+            potionTimers["Zdebug"]["time"] = GetTime() + 10;
             print("'" .. spellName .. "'' used by " .. unitName .. " (ID=" .. tostring(spellId) .. ", GUID=" .. guid .. ") passive? -> " .. tostring(isPassiveEffect));
         end
 
@@ -223,9 +259,16 @@ C_Timer.NewTicker(1, function()
 
     populateTimerPlaceholders();
 
-    msgFrame.text:SetText(buildText());
-    msgFrame:SetWidth(msgFrame.text:GetStringWidth() + 20);
-    msgFrame:SetHeight(msgFrame.text:GetStringHeight() + 20);
+    text = buildText();
+    
+    if string.len(text) == 0 then
+        msgFrame:Hide();
+    else
+        msgFrame.text:SetText(text);
+        msgFrame:SetWidth(msgFrame.text:GetStringWidth() + 20);
+        msgFrame:SetHeight(msgFrame.text:GetStringHeight() + 20);
+        msgFrame:Show();
+    end
 
     if frameData ~= nil and next(frameData) ~= nil and not dragging then
         msgFrame:ClearAllPoints();
